a0b855c363fb8a9d737b79e1aca86f0f1e3bd1c3
Artur Gottmann <artur.gottmann@kit.edu>
2020-01-09 18:32:38 +0100

    scripts/run.sh: fix to working hadd version; scripts/run_locally.py: allow also to run on a portal machine

---
+++ scripts/run_locally.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import tarfile
+import argparse
+from multiprocessing import Pool
+
+def execute_merging(sample):
+    tar = tarfile.open("merging.tar.gz", mode="r")
+    tar.extract("%s.sh"%sample)
+    os.system("bash %s.sh"%sample)
+
+def parseargs():
+    parser = argparse.ArgumentParser(description='Script to run merging shell scripts (created by scripts/merge_outputs.py) locally in parallel.')
+    parser.add_argument('--cores',type=int,help='Number of cores used for parallel processing. This option is required to be specified.',required=True)
+    return parser.parse_args()
+
+def main():
+    args = parseargs()
+    p = Pool(args.cores)
+    argumentfile = open("arguments.txt","r")
+    sample_names = [name.strip() for name in argumentfile.read().strip().split("\n")]
+    p.map(execute_merging,sample_names)
+
+if __name__ == "__main__":
+    main()
