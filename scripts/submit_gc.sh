1d4636f46c61653701b48fb4f2c2acb8d0bb924d
Artur Gottmann <artur.gottmann@kit.edu>
2020-01-10 13:26:15 +0100

    scripts/submit_gc.sh: determine number of jobs in a more stable way

--- scripts/submit_gc.sh
+++ scripts/submit_gc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp ${1} gc.conf
-NJOBS=$(cat arguments.txt | wc -l)
+NJOBS=$(awk 'END{print NR}' arguments.txt)
 sed -i "s/NJOBS/${NJOBS}/g" gc.conf
 echo "Execute:"
-echo "go.py gc.conf"
+echo "go.py gc.conf -Gc -m 3"
