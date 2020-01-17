660b049df000cb2f8c02559198e61644a0ba7b34
Artur Gottmann <artur.gottmann@kit.edu>
2020-01-17 15:50:45 +0100

    Remove grid-control, since not working up to now

--- scripts/setup_gc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR=$(dirname $(readlink -f $BASH_SOURCE))
-export PATH=$PATH:$SCRIPT_DIR/../grid-control:$SCRIPT_DIR/../grid-control/scripts
-export PYTHONPATH=$PYTHONPATH:$SCRIPT_DIR/../grid-control/packages
